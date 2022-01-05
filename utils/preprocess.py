f9032f14b80188e0ad3242728ca5d63290d78164
onebottlekick <gksqudcks97@gmail.com>
2022-01-05 11:34:30 +0900

    data preprocess
    - xml file to list object

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -2,6 +2,8 @@ import os
 from urllib import request
 import tarfile
 import zipfile
+from xml.etree import ElementTree
+import numpy as np
 
 
 def downloader(kind='data', root='../data'):
@@ -59,3 +61,41 @@ def make_data_path_list(root='../data/VOCdevkit/VOC2012/'):
             val_annotation_list.append(os.path.join(root, 'Annotations', f'{val_id.strip()}.xml'))
         
     return train_img_list, train_annotation_list, val_img_list, val_annotation_list
+
+
+class XML2List:
+    def __init__(self, classes):
+        self.classes = classes
+        
+    def __call__(self, xml_path, width, height):
+        result = []
+        xml = ElementTree.parse(xml_path).getroot()
+        
+        for obj in xml.iter('object'):
+            difficult = int(obj.find('difficult').text)
+            if difficult == 1:
+                continue
+            
+            bndbox = []
+            
+            name = obj.find('name').text.lower().strip()
+            bbox = obj.find('bndbox')
+
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+
+            for pt in pts:
+                cur_pxl = int(bbox.find(pt).text) - 1
+                
+                if pt == 'xmin' or pt == 'xmax':
+                    cur_pxl /= width
+                else:
+                    cur_pxl /= height
+                    
+                bndbox.append(cur_pxl)
+
+            label_idx = self.classes.index(name)
+            bndbox.append(label_idx)
+
+            result += [bndbox]
+
+        return np.array(result)
